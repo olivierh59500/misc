6e9b1b48e28bbd9485ba341094dcda26a6aff09a
Steven Peguero <steven@stevenpeguero.com>
2015-01-14 00:27:39 -0500

    Added Acer C720 post-install configs/scripts for Fedora 21 use.

---
+++ config/acerC720_postInstall/cros-sound-suspend.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Script normally belongs in /usr/lib/systemd/system-sleep/.
+
+case $1/$2 in
+    pre/*)
+        # Log time for suspend.
+        touch /tmp/suspend-test.log
+        echo -e "$(date); \c" >> /tmp/suspend-test.log
+        echo "OFF" >> /tmp/suspend-test.log
+
+        # Kill Adobe Flash flugin as it prevent suspend.
+        pgrep -f flashplayer | xargs kill >/dev/null 2>&1
+
+        # Unbind ehci for preventing error.
+        echo -n "0000:00:1d.0" | tee /sys/bus/pci/drivers/ehci-pci/unbind
+
+        # Unbind snd_hda_intel for sound.
+        echo -n "0000:00:1b.0" | tee /sys/bus/pci/drivers/snd_hda_intel/unbind
+        echo -n "0000:00:03.0" | tee /sys/bus/pci/drivers/snd_hda_intel/unbind
+        ;;
+    post/*)
+        # Bind ehci for preventing error.
+        echo -n "0000:00:1d.0" | tee /sys/bus/pci/drivers/ehci-pci/bind
+
+        # bind snd_hda_intel for sound.
+        echo -n "0000:00:1b.0" | tee /sys/bus/pci/drivers/snd_hda_intel/bind
+        echo -n "0000:00:03.0" | tee /sys/bus/pci/drivers/snd_hda_intel/bind
+
+        # Log time for suspend.
+        touch /tmp/suspend-test.log
+        echo -e "$(date); \c" >> /tmp/suspend-test.log
+        echo "ON" >> /tmp/suspend-test.log
+        ;;
+esac
---
+++ config/acerC720_postInstall/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+# Ensure to run this particular script as root.
+
+def main():
+
+	import os
+	import shutil
+	import sys
+	
+	files = {
+		'50-cros-touchpad.conf'		: '/etc/X11/xorg.conf.d',
+		'cros-acpi-wakeup.conf'		: '/etc/tmpfiles.d',
+		'cros-sound-suspend.sh'		: '/usr/lib/systemd/system-sleep',
+		'disable-touchpad-wakeup.conf'	: '/etc/tmpfiles.d'
+	}
+	
+	grub_configFile = "/etc/default/grub"
+	grub_bootParameters = 'GRUB_CMDLINE_LINUX_DEFAULT="quiet splash tpm_tis.force=1"'
+
+	# Copies configuration files and scripts to their respective directory.
+
+	for file in files.keys():
+		try:
+			print('Processing %s...' % file)
+			os.chown(file, 0, 0)
+			
+			if ".sh" in file:
+				os.chmod(file, 0o700)
+			else:
+				os.chmod(file, 0o600)
+			
+			shutil.copy(file, files[file])
+		
+		except OSError as why:
+			if why.args[0] == 1:
+				print('ERROR: Could not change owner of "%s" to root.' % (file), file=sys.stderr)
+			else:
+				print('ERROR: Could not copy "%s". Details: %s.' % (file, why), file=sys.stderr)
+			continue
+			
+	# Adds a specific boot parameter to GRUB, which is required for proper suspending.
+	
+	try:
+		print('Adding a specific boot parameter to %s...' % grub_configFile)
+		
+		with open(grub_configFile, "a") as myfile:
+			myfile.write("\n%s" % grub_bootParameters)
+			print('WARN: Inspect "%s" before generating a grub2 configuration file.' % grub_configFile)
+	except OSError as why:
+		print('ERROR: Could not edit "%s". Details: %s.' % (grub_configFile, why), file=sys.stderr)
+		print('\nProcess completed with a non-zero exit status.', file=sys.stderr)
+		exit(1)
+
+	print('\nProcess complete. Reboot for modifications to take into effect.')
+	exit(0)
+
+if __name__ == '__main__':
+	main()
+
