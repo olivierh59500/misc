1155d0f3f435da834ce6d46f42683a8118cdb88b
Steven Peguero <steven@stevenpeguero.com>
2015-02-02 17:20:34 -0500

    Add default PowerShell profile configuration

--- config/profile_powershell/module/Repeat-Command.psm1
+++ config/profile_powershell/module/Repeat-Command.psm1
@@ -1,9 +1,9 @@
 function Repeat-Command {
-	
+
 	<#
 		.SYNOPSIS
 		A PowerShell variant of the 'watch' Unix command, used for continuously invoking commands every specified amount of seconds.
-		
+
 		.EXAMPLE
 		Repeat-Command 'ls'
 		Invokes the specified command, ls, every second.
@@ -11,12 +11,12 @@ function Repeat-Command {
 		.EXAMPLE
 		Repeat-Command 'ls' 2
 		Invokes the specified command, ls, every two seconds.
-		
+
 		.EXAMPLE
 		Repeat-Command 'ls' 2 -WithoutClearingBuffer
 		Repeat-Command 'ls' 2 -w
 		Invokes the specified command, ls, every two seconds, without clearing a console's buffer.
-		
+
 		.EXAMPLE
 		Repeat-Command 'ls' 2 -TerminateIfError
 		Repeat-Command 'ls' 2 -t
@@ -24,57 +24,57 @@ function Repeat-Command {
 	#>
 
 	[CmdletBinding()]
-	
+
 	Param (
 		[Parameter(Mandatory=$True)]
 		[String]
 		$Command,
-		
+
 		[Parameter(Mandatory=$False)]
 		[Int64]
 		$Seconds = 1,
-		
+
 		[Parameter(Mandatory=$False)]
 		[Alias("w")]
 		[Switch]
 		$WithoutClearingBuffer,
-		
+
 		[Parameter(Mandatory=$False)]
 		[Alias("t")]
 		[Switch]
 		$TerminateIfError
 	)
-	
+
 	Begin {
-		$HeadsUpDisplay_Text = "Repeat-Command: '" + $Command + "' every " + $Seconds + " second(s)"
-		$HeadsUpDisplay_Separator = "-" * $HeadsUpDisplay_Text.Length + "`n"
-		$HeadsUpDisplay = @($HeadsUpDisplay_Text, $HeadsUpDisplay_Separator)
-		
+		$HeadsUpDisplay_Text       = "Repeat-Command: '" + $Command + "' every " + $Seconds + " second(s)"
+		$HeadsUpDisplay_Separator  = "-" * $HeadsUpDisplay_Text.Length + "`n"
+		$HeadsUpDisplay            = @($HeadsUpDisplay_Text, $HeadsUpDisplay_Separator)
+
 		$Host.UI.RawUI.WindowTitle = $Host.UI.RawUI.WindowTitle + " (Repeat-Command)"
 	}
-	
+
 	Process {
 		do {
 			if ($WithoutClearingBuffer -eq $False) {
 				Clear-Host
 				Write-Output $HeadsUpDisplay
 			}
-			
+
 			try {
 				Invoke-Expression $Command | Out-Default
 			}
-			
+
 			catch [Exception] {
 				Write-Host -ForegroundColor Red ("ERROR: " + $_.Exception.Message)
-				
+
 				if ($TerminateIfError -eq $True) {
 					break
 				}
 			}
-			
+
 			Start-Sleep -Seconds $Seconds
 		}
-		
+
 		until (
 			$Null
 		)
---
+++ config/profile_powershell/profile.ps1
@@ -0,0 +1,98 @@
+
+$ErrorActionPreference = "Stop"
+
+function Initialize-Session {
+
+	$SessionDirectoryPath = $PSScriptRoot
+#	$ConfigDirectoryPath  = $SessionDirectoryPath + "\config" <-- Remaining for future implementation.
+	$ModuleDirectoryPath  = $SessionDirectoryPath + "\module"
+
+	# ---- FUNCTIONS ----
+
+	function ReportStatus {
+
+		Param (
+			[String]
+			$Message,
+
+			[String]
+			$Type
+		)
+
+		if ($Type -eq "Okay") {
+			Write-Host -NoNewLine -ForegroundColor Black -BackgroundColor Green -Object " + "
+		}
+
+		if ($Type -eq "Fail") {
+			Write-Host -NoNewLine -ForegroundColor White -BackgroundColor Red -Object " - "
+		}
+
+		Write-Output $Message
+	}
+
+	# ---- SHELL CONFIGURATION ----
+
+	$Host.UI.RawUI.BackgroundColor = "Black"
+	$Host.UI.RawUI.ForegroundColor = "Green"
+
+	Clear-Host
+
+	function Global:Prompt {
+
+		[ArrayList]$Prompt = [Ordered]@{
+			"`nPS "                                                   = "Cyan"
+			"["                                                       = "White"
+			($env:UserName + '@' + $env:ComputerName.ToLower() + " ") = "Cyan"
+			(Get-Location).ProviderPath                               = "Red"
+			"]"                                                       = "White"
+		}
+
+		Write-Output "`n> "
+
+		foreach ($Item in $Prompt.Keys) {
+			Write-Host -NoNewLine -ForegroundColor $Prompt[$Item] -Object $Item
+			[String]$HostTitleBar += $Item
+		}
+
+		$Host.UI.RawUI.WindowTitle = $HostTitleBar
+	}
+
+	# ---- MODULE IMPORTATION ----
+
+	Write-Host -Object "" # Leaves space between name of first imported module and title bar.
+
+	if (!(Test-Path -Path $ModuleDirectoryPath)) {
+		try {
+			New-Item -Name "module" -Path $SessionDirectoryPath -ItemType Directory -Force | Out-Null
+			ReportStatus -Type "Okay" -Message (" Created module directory at '" + $ModuleDirectoryPath + "'.")
+		}
+
+		catch {
+			ReportStatus -Type "Fail" -Message (" An error occurred when attempting to create module directory at '" + $ModuleDirectoryPath + "' :`n`n" + $_.Exception.Message)
+		}
+	}
+
+	else {
+		Get-Module | ? {$_.ModuleBase -like "$SessionDirectoryPath*" -and $_.Name -ne $Session_ProfileName} | Remove-Module
+		$ModuleList = (Get-ChildItem -Path $ModuleDirectoryPath -Filter "*.psm1" -Recurse)
+
+		if ($ModuleList) {
+			foreach ($Module in $ModuleList) {
+				try {
+					Import-Module -Name $Module.FullName -DisableNameChecking -Force
+					ReportStatus -Type "Okay" -Message (" " + $Module.BaseName)
+				}
+
+				catch {
+					ReportStatus -Type "Fail" -Message (" " + $Module.BaseName + " :`n`n" + $_.Exception.Message + "`n")
+				}
+			}
+		}
+
+		else {
+			ReportStatus -Type "Fail" -Message (" No modules available for importation.")
+		}
+	}
+}
+
+Initalize-Session
